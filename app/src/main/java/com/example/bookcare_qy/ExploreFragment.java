e9ca5e3b126409980760b80ebec8995140ccf0d7
Zyinggg635 <sanicelzx@gmail.com>
2025-12-28 22:47:53 +0800

    Fix ExploreFragment

--- app/src/main/java/com/example/bookcare_qy/ExploreFragment.java
+++ app/src/main/java/com/example/bookcare_qy/ExploreFragment.java
@@ -8,14 +8,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import com.example.bookcare_qy.R;
+import com.example.bookcare_qy.databinding.FragmentExploreBinding;
 
 public class ExploreFragment extends Fragment {
+
+    private FragmentExploreBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // This connects the Java class to its XML layout file.
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        binding = FragmentExploreBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        binding.btnMyListings.setOnClickListener(v -> 
+            navController.navigate(R.id.action_navigation_explore_to_my_Listings));
+
+        binding.btnUploadBook.setOnClickListener(v -> 
+            navController.navigate(R.id.action_navigation_explore_to_add_New_Book));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
